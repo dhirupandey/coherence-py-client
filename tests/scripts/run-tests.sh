02cb263832a659482dc38507ac3ac92bdb64f2af
Dhiru Pandey <dhiru.pandey@oracle.com>
2023-07-25 16:10:34 -0700

    Debug information for time_out error seen randomly

--- tests/scripts/run-tests.sh
+++ tests/scripts/run-tests.sh
@@ -17,12 +17,14 @@ mkdir -p logs
 
 echo "Coherence CE 22.06.5"
 COHERENCE_CLIENT_REQUEST_TIMEOUT=180.0 \
+  COHERENCE_IO_JSON_DEBUG=false \
   make clean test-cluster-shutdown remove-app-images build-test-images test-cluster-startup just-wait test
   make dump-logs test-cluster-shutdown
   mv ./tests/utils/run-logs.txt ./logs/run-logs-clear.txt
 
 echo "Coherence CE 22.06.5 with SSL"
 RUN_SECURE=true COHERENCE_IGNORE_INVALID_CERTS=true \
+  COHERENCE_IO_JSON_DEBUG=false \
   COHERENCE_TLS_CERTS_PATH=$(pwd)/tests/utils/certs/guardians-ca.crt \
   COHERENCE_TLS_CLIENT_CERT=$(pwd)/tests/utils/certs/star-lord.crt \
   COHERENCE_TLS_CLIENT_KEY=$(pwd)/tests/utils/certs/star-lord.pem \
